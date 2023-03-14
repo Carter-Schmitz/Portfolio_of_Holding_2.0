6c50a8b259ad400b6216450d64f4d211418779be
Carter_Schmitz <carterschmitz95@gmail.com>
2023-03-13 23:45:41 -0500

    added input form for contacting me

--- portfolio_of_holding2.0/src/components/pages/Contact.js
+++ portfolio_of_holding2.0/src/components/pages/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
+
 export default function Contact() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className='contactForm'>
+      <h1 className='contactForm'>Contact Me</h1>
+      <form className='contactForm' action="mailto:carterschmitz95@gmail.com" method="post" enctype="text/plain">
+        <label> Name: </label>
+        <input className='contactForm' type="text" id="name"></input>
+        <label> Email: </label>
+        <input className='contactForm' type="email" id="email"></input>
+        <label> Your Message Here: </label>
+        <input className='contactForm' type="text" id="message" ></input>
+        <input className='contactForm' type="submit" value="Send"></input>
+      </form>
     </div>
   );
 }
