ecc55b99892ca788b62ad47f565a457eb89ab3fe
Carter_Schmitz <carterschmitz95@gmail.com>
2023-06-18 20:08:21 -0500

    profile pic

--- portfolio_of_holding2.0/src/components/pages/About.js
+++ portfolio_of_holding2.0/src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='about'>
       <h1>About Me</h1>
-      <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/120301005_10221372359033333_2953361097939317825_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=BVCFIDQunqEAX_jcDgf&_nc_oc=AQncra9ore3aCHtPiWtEtAj9VX6xNqK0_CfBNtarBruAP3Cy4czzUazphv4I8j37pUoLPIkPLT7jIDUPOcLRgSEX&_nc_ht=scontent-dfw5-1.xx&oh=00_AfDpEujutK6BfYZNVOaW4GubephSN-CIfMXBPnS0ffbRsg&oe=6477E593" alt="One Handsome Devil"></img>
+      <img src="portfolio_of_holding2.0\public\assets\120301005_10221372359033333_2953361097939317825_n (1).jpg" alt="One Handsome Devil"></img>
       <p>Full-Stack Web Developer. Proficient with the most current technologies used to build web based applications. </p>
         <p>Always a team player, I have worked with dozens of separate groups over the years. On projects with as few as
             3 people, to a group of 30. I understand the nuances and challenging dynamics that can come with working
